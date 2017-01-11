128f323a77293837ffe6fa7d573cebfbc8e85e9b
Shevchenko Yuriy <150030@mail.ru>
2017-01-11 20:20:22 +1000

    Вынес считывание файла в отдельную функцию

--- parallel_wl.c
+++ parallel_wl.c
@@ -16,27 +16,36 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+int n; // количество спинов
+signed char *spins; //массив направления спинов. По умолчанию +1. n - число считанных спинов (число незакомментированных строк в csv-файле).
+unsigned short *a_neighbours; //число соседей каждого спина. Считается как число энергий в соответствующей строке в csv-файле.
+unsigned short *neighbours; // соседи каждого спина
+unsigned int *sequencies; //для каждого спина описывает, с какого ключа в массиве energies[] начинают описываться парные энергии
+float *energies; //сами энергии из файла. Описывается как одномерный массив. Длина массива - число парных энергий в csv-файле.
+float emin, emax; //минимумы и максимумы энергии
+float e; //текущая энергия системы
+
+#define PRECISION 3 //Сколько знаков учитывать в энергии после запятой
+
+
+void readCSV(char* filename);
+void rotate(int spin); // Считает энергию системы
+void complete();
+
+
 int main(void)
 {
-    int n=0; // количество спинов
-    float *testmas;
-    testmas=(float *) malloc(100*sizeof(float));
-
-    FILE *f = fopen("csv_examples/simplest_exmple.csv","r");
-    fscanf(f,"%[,]%s",testmas);
 
 
     fclose(f);
 
     printf("Testmass %s",testmas);
-    signed char *spins; //массив направления спинов. По умолчанию +1. n - число считанных спинов (число незакомментированных строк в csv-файле).
-    unsigned short *a_neighbours; //число соседей каждого спина. Считается как число энергий в соответствующей строке в csv-файле.
-    unsigned short *neighbours; // соседи каждого спина
-    unsigned int *sequencies; //для каждого спина описывает, с какого ключа в массиве energies[] начинают описываться парные энергии
-    float *energies; //сами энергии из файла. Описывается как одномерный массив. Длина массива - число парных энергий в csv-файле.
+}
 
 
-    /// выделяем память массивам
+void readCSV(char *filename){
+    // reserve memory for arrays
     spins=(signed char *) malloc(n*sizeof(signed char));
     a_neighbours=(unsigned short *) malloc(n*sizeof(unsigned short));
     neighbours=(unsigned short *) malloc(n*sizeof(unsigned short));     //поменять размер
@@ -44,14 +53,26 @@ int main(void)
     energies=(float *) malloc(n*sizeof(float ));                        //поменять размер
 
 
+    // read data
 
 
-    /// стираем массивы
+    float *testmas;
+
+    FILE *f = fopen("csv_examples/simplest_exmple.csv","r");
+    fscanf(f,"%[,]%s",testmas);
+    testmas=(float *) malloc(100*sizeof(float));
+
+}
+
+void rotate(int spin){
+
+}
+
+void complete(){
+    // clean arrays
     free(spins);
     free(a_neighbours);
     free(neighbours);
     free(sequencies);
     free(energies);
-
-
 }
